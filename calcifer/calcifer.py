b7862de4051a15e5a9ee88f7afd7679fab6f4e08
howellcalcifer <rick.taylor@york.ac.uk>
2023-05-28 11:46:03 +0100

    rm circular import

--- calcifer/calcifer.py
+++ calcifer/calcifer.py
@@ -6,4 +6,4 @@ class Calcifer:
     def description(self) -> Scene:
         return Scene("You are Calcifer. You have a thin blue face, a thin blue nose, curly green flames for hair" 
                      "and eyebrows, a purple flaming mouth, and savage teeth. Your eyes are orange flames with" 
-                     "purple pupils. You do not have any evident lower body.", items=[])
+                     "purple pupils. You do not have any evident lower body.")
--- world/scene.py
+++ world/scene.py
@@ -1,12 +1,8 @@
 from __future__ import annotations
 
 import dataclasses
-from typing import Collection
-
-from world.items import Item
 
 
 @dataclasses.dataclass
 class Scene:
     text: str
-    items: Collection[Item]
